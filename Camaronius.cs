bf9ae4b895d8c1e75749f91c382f4fef2bc8e3ff
matias19970 <43451891+matias19970@users.noreply.github.com>
2018-10-01 23:51:45 -0300

    Update Camaronius.cs

--- Camaronius.cs
+++ Camaronius.cs
@@ -8,5 +8,11 @@ namespace Bitmon
 {
     class Camaronius : bitmon
     {
+        string nombre;
+        string tipo;
+        int vida;
+        int estamina;
+        int ataque;
+        int defensa;
     }
 }
